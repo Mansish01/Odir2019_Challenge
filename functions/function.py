b45bcf15e15afca5538b469a1818afe9cfb564d3
Mansish01 <gyawalimanish12345@gmail.com>
2024-01-21 15:53:42 +0545

    updated path

--- functions/function.py
+++ functions/function.py
@@ -1,24 +1,24 @@
 
 import csv
 
-def read_csv(csv_file):
-    images= []
+def read_csv(csv_file, has_labels=True):
+    image_paths = []
     labels = []
-    
 
-    with open(csv_file, 'r') as fil:
-        reader = csv.reader(fil)
-        next(reader)
+    with open(csv_file, "r") as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        
+        fundus_index = header.index("image_fundus")
+
         for row in reader:
-            image_path = row[1]
-            label = row[2] if len(row) > 2 else None
+            image_paths.append(row[fundus_index])
 
-            images.append(image_path)
-            labels.append(label)
-       
-    
-        return images ,labels
-    
+            if has_labels:
+                label_index = header.index("label")
+                labels.append(row[label_index])
+
+    return image_paths, labels if has_labels else image_paths
     
     
 if __name__ == "__main__":
